f54305761c141936c87b04771c8f28ab3663d2b4
Antarip chatterjee <85601268+AntaripChat@users.noreply.github.com>
2022-10-26 20:04:18 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+const PORT = 2000;
+
+app.use(bodyparser.json());
+const data = [
+    {
+        "Name": "Antarip Chatterjee",
+        "Class": 12,
+        "age":17
+    }
+];
+
+app.get('/',(req,res)=>{
+    res.send(data);
+});
+app.post('/',(req,res)=>{
+    const dat = req.body;
+    data.push(dat)
+    //console.log(req.body);
+    res.send(`Hello ${dat.Name}`)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Running on ${PORT}`);
+})
